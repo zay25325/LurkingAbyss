3a8d3dc78cd0fdc221b0a2c97500b87a55373b5c
Andrew3369 <andrewjanosbabos@gmail.com>
2025-01-23 00:37:24 -0500

    MainMenuUI.cs script Changes
    - Commented out Start and Void functions
    - Created event listeners for the rest of the buttons along with a debug message.

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -7,28 +7,51 @@ using UnityEngine.UIElements;
 public class MainMenuUI : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
-
+    //}
 
     void OnEnable()
     {
         var rootElement = GetComponent<UIDocument>().rootVisualElement;
-        var startButton = rootElement.Q<Button>("PlayBtn"); // make sure name is the same in UI Builder
 
-        startButton.clicked += () =>
+        // make sure name is the same in UI Builder
+        var StartButton = rootElement.Q<Button>("PlayBtn");
+        var LogsButton = rootElement.Q<Button>("LogsBtn");
+        var SettingsButton = rootElement.Q<Button>("SettingsBtn");
+        var QuitButton = rootElement.Q<Button>("QuitBtn");
+
+        StartButton.clicked += () =>
         {
             Debug.Log("Start Button Clicked");
             //UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
         };
-    
+
+        LogsButton.clicked += () =>
+        {
+            Debug.Log("Logs Button Clicked");
+            //UnityEngine.SceneManagement.SceneManager.LoadScene("LogsScene");
+        };
+
+        SettingsButton.clicked += () => 
+        {
+            Debug.Log("Settings Button Clicked");
+            //UnityEngine.SceneManagement.SceneManager.LoadScene("SettingsScene");
+        };
+
+        QuitButton.clicked += () => 
+        {
+            Debug.Log("Quit Button Clicked");
+            // not sure how to quit :p
+
+        };
+
     }
 }
