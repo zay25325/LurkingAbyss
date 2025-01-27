aad966ea804883fd197ce96537a81d2687013fa0
Andrew3369 <andrewjanosbabos@gmail.com>
2025-01-27 17:25:35 -0500

    Prefab of a Simple HUD

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Slider intSlider;
+
+    public void SetMaxHealth(int health)
     {
-        
+        intSlider.maxValue = health;
+        intSlider.value = health;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetHealth(int health)
     {
-        
+        intSlider.value = health;
     }
 }
