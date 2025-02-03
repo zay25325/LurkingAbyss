ac3e89aab5cabc19260cc8deb39f15722f7452d9
Andrew3369 <andrewjanosbabos@gmail.com>
2025-02-02 20:11:04 -0500

    Updates on LogsMenu UI
    - Added more GUI visuals and features
    - Updated the script code as a start for logic on player finding and discovering monsters & items

--- Assets/Scripts/LogsMenuUI.cs
+++ Assets/Scripts/LogsMenuUI.cs
@@ -1,30 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
+enum ItemType
+{
+    Item,
+    Monster
+}
+
+
 public class LogsMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    //void Start()
-    //{
+    private class LogItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool isDiscovered { get; set; }
+        public ItemType itemType { get; set; }
 
-    //}
+        private LogItem(string name, string description, ItemType itemType)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.isDiscovered = false;
+            this.itemType = itemType;
+        }
+    }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
+    // list so we can store the info of the logged items
+    private List<LogItem> loggedItems = new();
 
-    //}
+    // Visual Unity UI Toolkit variabels
+    private VisualElement rootElement;
+    private VisualElement container;
+
 
     void OnEnable()
     {
-        var rootElement = GetComponent<UIDocument>().rootVisualElement;
+        rootElement = GetComponent<UIDocument>().rootVisualElement;
 
         var BackButton = rootElement.Q<Button>("BackBtn");
         BackButton.clicked += () =>
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
         };
+
+        container = rootElement.Q<VisualElement>("Logs-Container");
+
+
+
+        InitializeLogItems();
+    }
+
+    private bool InitializeLogItems()
+    {
+        try
+        {
+            // log items here
+            return true;
+        }
+        catch (System.Exception error)
+        {
+            Debug.Log(error.Message);
+            return false;
+        }
+    }
+
+
+    private void ShowDescription()
+    {
+        
     }
 }
