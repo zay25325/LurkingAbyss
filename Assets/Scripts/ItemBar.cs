0d50c7ddadb7adb760be588879e77aece82de516
Andrew3369 <andrewjanosbabos@gmail.com>
2025-03-15 23:56:10 -0400

    Quick fixes to the hot bar issues

--- Assets/Scripts/ItemBar.cs
+++ Assets/Scripts/ItemBar.cs
@@ -8,9 +8,9 @@ using UnityEngine.UIElements;
 public class ItemBar : MonoBehaviour
 {
 
-    private HorizontalLayoutGroup itemBarHUD; // for displayting the images of the childs
     private Inventory playerInventory; // temp stash for items so we can view them
-    
+    public HorizontalLayoutGroup itemBarHUD; // for displayting the images of the childs
+    public GameObject itemPrefab;
 
     private void Start()
     {
@@ -20,7 +20,7 @@ public class ItemBar : MonoBehaviour
 
 
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null)
+        if (player == null)
         {
             playerInventory = player.GetComponent<Inventory>();
             if (playerInventory != null)
@@ -33,31 +33,53 @@ public class ItemBar : MonoBehaviour
         }
         else
             Debug.Log("Player inventory component wasnt found");
+
+        if (itemPrefab != null)
+        {
+            Debug.LogError("ItemPrefab is not assigned in Itembar");
+        }
     }
 
     private void UpdateInventoryUI()
     {
         //Debug.Log("UpdateInventoryUI called!"); // debugging
-
         if (itemBarHUD == null)
         {
             Debug.LogError("ItemBarHUD is not assigned!");
             return;
         }
 
-        // delete prev items
+        // delete prev items/images
         foreach (Transform child in itemBarHUD.transform)
             Destroy(child.gameObject);
 
         List<Item> items = playerInventory.GetItems();
 
         foreach(Item item in items) // debug
-        {
             Debug.Log($"Item in inventory: {item}");
+
+        if (items.Count == 3)
+            Debug.Log("All slots used");
+
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject slot = Instantiate(itemPrefab, itemBarHUD.transform);
+
+            //Image itemImage = slot.GetComponent<Image>(); // 'Image' keyword wasnt being recognized
+            UnityEngine.UI.Image itemImage = slot.GetComponentInChildren<UnityEngine.UI.Image>();
+            Debug.Log("About to add images to HUD");
+            if (i < items.Count)
+            {
+                if (items[i].ItemIcon != null)
+                {
+                    itemImage.sprite = items[i].ItemIcon;
+                    Debug.Log($"Added sprite image for item: {items[i].ItemName}");
+                }
+                else
+                    Debug.Log($"item: {items[i].name} does not have a sprite attached");
+            }
+            else
+                Debug.Log("Could not find items");
         }
-
-        // somehow get the sprite image
-
-        //throw new NotImplementedException();
     }
 }
