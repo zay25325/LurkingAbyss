fc75fb7a41603e2b4075dc0328b2cd87925c60ef
Andrew3369 <andrewjanosbabos@gmail.com>
2025-03-21 02:09:18 -0400

    Pause Menu Updates
    - Can send the player back to the main menu
    - Player can press Esc to pause, and again to unpause
    - Resume button unpauses the game
    - Game does not continue once the player pauses, everything is set frozen

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject pauseMenu; // the canvas refernce
+
+    public Button resumeBtn, quitBtn;
+
+    public bool isPaused;
+
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
+
+        // event listeners in the pause menu
+        resumeBtn.onClick.AddListener(ResumeGame);
+        quitBtn.onClick.AddListener(SendToMainMenu);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f; // stops all animations and what not
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void SendToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
