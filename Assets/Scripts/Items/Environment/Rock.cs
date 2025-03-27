33bebb35ccdc4d047267f8f1926affe3fad0486f
ejuracic <ejuracic9193@conestogac.on.ca>
2025-03-27 13:56:08 -0400

    CAP-118 Rock Bug Fix
    Rock doesn't damage player. Radius is more accurate and can now disable Bear Traps. Re-Added code to reduce Rock Item Charge so it is now one time use

--- Assets/Scripts/Items/Environment/Rock.cs
+++ Assets/Scripts/Items/Environment/Rock.cs
@@ -58,7 +58,7 @@ public class Rock : Item
         if (CanUseItem())
         {
             StartCoroutine(Throw());
-            //ReduceItemCharge();
+            ReduceItemCharge();
             DestroyItem(ItemObject);
         }
         else
